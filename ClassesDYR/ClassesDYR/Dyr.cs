daa83a12b82aa975d2c9159a8bb892684852de23
Frederik Koch <frederikhkoch@gmail.com>
2018-08-31 10:50:01 +0200

    Final

---
+++ ClassesDYR/ClassesDYR/Dyr.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDYR
+{
+    abstract class Dyr
+    {
+        public string art { get; set; }
+        public string foder { get; set; }
+        public string lyd { get; set; }
+        public double vaegt { get; set; }
+
+        public string udskrivDyr()
+        {
+            return "Art: " + art + " "+ "Foder: " + foder + " "+ "lyd: " + lyd + " "+ "Vægt: " + vaegt;
+        }
+    }
+
+    class Elefant : Dyr
+    {
+        public Elefant(string f, double v)
+        {
+            this.vaegt = v;
+            this.foder = f;
+            this.lyd = "Trut";
+            this.art = "African";
+        }
+    }
+
+    class Hund : Dyr
+    {
+        public Hund(string f, double v)
+        {
+            this.vaegt = v;
+            this.foder = f;
+            this.lyd = "Vuf";
+            this.art = "Lupin";
+        }
+    }
+
+    class Kat : Dyr
+    {
+        public Kat(string f, double v)
+        {
+            this.vaegt = v;
+            this.foder = f;
+            this.lyd = "Miaw";
+            this.art = "Felis catos";
+        }
+    }
+
+    
+}
---
+++ ClassesDYR/ClassesDYR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDYR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dyr = new List<string>();
+
+            Dyr elefant = new Elefant("Vegetarisk", 1500.25);
+            Dyr hund = new Hund("Hundefoder", 25.54);
+            Dyr kat = new Kat("Kattemad", 5.45);
+            dyr.Add(elefant.udskrivDyr());
+            dyr.Add(hund.udskrivDyr());
+            dyr.Add(kat.udskrivDyr());
+            foreach (var animal in dyr)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.ReadLine();
+        }
+    }
+}
